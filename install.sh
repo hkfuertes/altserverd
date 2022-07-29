6b8dc2c1f0e43d1763320071b825f19a43d500bb
Miguel Fuertes <hkfuertes@gmail.com>
2022-07-29 16:37:33 +0200

    changes

--- install.sh
+++ install.sh
@@ -11,16 +11,19 @@ if [[ ! -e "AltStore.ipa" ]]; then
     echo "[i] Downloading AltStore.ipa"
     curl -# -L https://cdn.altstore.io/file/altstore/apps/altstore/1_4_9.ipa > AltStore.ipa
     echo "[i] AltStore.ipa Downloaded!"
+    echo
 fi
 if [[ ! -e "netmuxd" ]]; then
     echo "[i] Building netmuxd"
-    docker-compose up -d netmuxd
+    docker-compose up netmuxd
     echo "[i] netmuxd built!"
+    echo
 fi
-if [[ ! -e "Altserver" ]]; then
+if [[ ! -e "AltServer" ]]; then
     echo "[i] Downloading AltServer"
     curl -# -L https://github.com/NyaMisty/AltServer-Linux/releases/download/v0.0.5/AltServer-`arch` > AltServer
     echo "[i] AltServer Downloaded!"
+    echo
 fi
 if [[ "stat AltServer | grep -- '-rw-r--r--'" != "" ]] ; then
     chmod +x AltServer
@@ -31,8 +34,12 @@ fi
 if [[ ! -e "AltServer.service" ]] ; then
     sed 's@<path>@'"$PWD"'@' AltServer.service.dist > AltServer.service
     echo "[i] Altserver service file created, link it to systemd to enable|start|stop it!"
+    echo
 fi
 if [[ ! -e "netmuxd.service" ]] ; then
     sed 's@<path>@'"$PWD"'@' netmuxd.service.dist > netmuxd.service
     echo "[i] netmux service file created, link it to systemd to enable|start|stop it!"
+    echo
 fi
+
+echo "[i] All Tasks done!"
