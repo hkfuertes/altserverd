a9cbd7ba9e2b659e14523edb9b8adfe0e33ef099
Miguel Fuertes <hkfuertes@gmail.com>
2022-07-31 23:51:11 +0200

    changes

--- altserver.sh
+++ altserver.sh
@@ -7,4 +7,5 @@ sudo docker run \
     -v /dev/bus/usb:/dev/bus/usb \
     -v /var/lib/lockdown:/var/lib/lockdown \
     -v /var/run:/var/run \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
     -it altserver $@
